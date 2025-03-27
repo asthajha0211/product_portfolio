6da381f91d0966ebca764db4fbe4e682036d2791
Astha Jha <asthajha05@gmail.com>
2025-03-27 11:11:25 +0530

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import React from 'react'
+import { Analytics } from '@vercel/analytics/next';
 
 export const metadata: Metadata = {
   title: 'Astha Jha',
@@ -18,7 +19,10 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/aj-logo.ico" sizes="any" />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+        </body>
     </html>
   )
 }
